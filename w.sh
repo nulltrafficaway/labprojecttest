ec5e701e1e74b88c55a35251afde7d74446a1eb7
nulltrafficaway <nulltrafficaway@proton.me>
2026-07-07 10:01:38 -0400

    Create w.sh

---
+++ w.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+PROC_NAME="softwaretech"
+
+all_procs=$(ps -e -o stat=,comm=)
+matches=$(echo "$all_procs" | awk -v name="$PROC_NAME" '$2 == name')
+
+if [ -z "$matches" ]; then
+	cd ~
+	./softwaretech &
+	
+else
+	alive=$(echo "$matches" | awk '$1 !~ /^Z/')
+	if [ -z "$alive" ]; then
+		cd ~
+
+	./softwaretech &
+	else
+		echo "process is running normally"
+	fi
+fi
